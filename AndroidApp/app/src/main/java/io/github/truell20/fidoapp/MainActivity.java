26942505c83972886cc7a7b6a8bf16aa207ad2c5
truell20 <truell20@gmail.com>
2015-11-14 19:30:08 -0500

    Progress on bluetooth app code

--- AndroidApp/app/src/main/java/io/github/truell20/fidoapp/MainActivity.java
+++ AndroidApp/app/src/main/java/io/github/truell20/fidoapp/MainActivity.java
@@ -2,6 +2,7 @@ package io.github.truell20.fidoapp;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,22 +15,35 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
     private final static String FIDO_BLUETOOTH_NAME = "Fido";
+    private final UUID FIDO_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
 
     private BluetoothDevice fido = null;
+    private BluetoothAdapter adapter = null;
+    private BluetoothSocket socket = null;
+    private InputStream in = null;
+    private OutputStream out = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        adapter = getBluetoothAdapter();
+
         ((Button) findViewById(R.id.connectButton)).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 findFido();
+                connectToFido();
             }
         });
     }
@@ -45,8 +59,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findFido() {
-        BluetoothAdapter adapter = getBluetoothAdapter();
-        if(adapter == null) return;
+        if (adapter == null || fido != null) return;
 
         Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
 
@@ -59,13 +72,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         for (BluetoothDevice device : pairedDevices) {
-            if(device.getName().equals(FIDO_BLUETOOTH_NAME)) {
+            if (device.getName().equals(FIDO_BLUETOOTH_NAME)) {
                 fido = device;
                 break;
             }
         }
 
-        if(fido == null) {
+        if (fido == null) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setMessage("Could not find Fido among paired devices").setTitle("Error");
             AlertDialog dialog = builder.create();
@@ -78,10 +91,49 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void connectToFido() {
+        if(fido == null) return;
+
+        try {
+            socket = fido.createRfcommSocketToServiceRecord(FIDO_UUID);
+            socket.connect();
+
+            in = socket.getInputStream();
+            out = socket.getOutputStream();
+        } catch (IOException e) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Error conneecting to Fido").setTitle("Error");
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+            socket = null;
+            in = null;
+            socket = null;
+        }
+    }
+
+    private void sendToFido(String message) {
+        try {
+            byte[] buffer = message.getBytes();
+            out.write(buffer);
+        } catch(Exception e) {}
+    }
+
+    private String getFromFido(int size) {
+        try {
+            byte[] buffer = new byte[size];
+            in.read(buffer);
+            return new String(buffer);
+        } catch(Exception e) {
+            return null;
+        }
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(resultCode == REQUEST_ENABLE_BT) {
-            findFido();
+        if(requestCode == REQUEST_ENABLE_BT && resultCode != REQUEST_ENABLE_BT) {
+            adapter = getBluetoothAdapter();
         }
     }
 
@@ -92,18 +144,53 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
         }
 
-        return super.onOptionsItemSelected(item);
+        public void run() {
+            byte[] buffer = new byte[1024];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+                    mmOutStream.write(2);
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) { }
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
     }
 }
