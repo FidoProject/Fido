ae412dcbe51b8c23be37ee0dcfaed10640eb71eb
truell20 <truell20@gmail.com>
2016-02-27 23:14:03 -0500

    More boiler plate

--- App/www/js/controllers.js
+++ App/www/js/controllers.js
@@ -1,7 +1,14 @@
-angular.module('Fido.controllers', [])
+angular.module('Fido.controllers', ["cordova-plugin-bluetooth-serial"])
 
 .controller('LoginCtrl', function($scope, $cordovaBarcodeScanner,$location) {
-	
+	bluetoothSerial.discoverUnpaired(function(devices) {
+		console.log(devices);
+	}, function() {
+		console.log("fail")
+	});
+	$scope.connectToFido = function() {
+
+	};
 })
 
 .controller('TestCtrl', function($scope, $cordovaBarcodeScanner,$location) {
