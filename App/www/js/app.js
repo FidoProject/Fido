cc053ce51b771b1763ff7c34e772fa5b5c230c72
truell20 <truell20@gmail.com>
2016-02-27 21:27:54 -0500

    Added Ionic app

---
+++ App/www/js/app.js
@@ -0,0 +1,49 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('Fido', ['ionic', 'ngCordova', 'Fido.controllers'])
+
+.run(function($ionicPlatform) {
+	$ionicPlatform.ready(function() {
+		if(window.cordova && window.cordova.plugins.Keyboard) {
+			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+			// for form inputs)
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+			// Don't remove this line unless you know what you are doing. It stops the viewport
+			// from snapping when text inputs are focused. Ionic handles this internally for
+			// a much nicer keyboard experience.
+			cordova.plugins.Keyboard.disableScroll(true);
+		}
+		if(window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+	});
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+	// Ionic uses AngularUI Router which uses the concept of states
+	// Learn more here: https://github.com/angular-ui/ui-router
+	// Set up the various states which the app can be in.
+	// Each state's controller can be found in controllers.js
+	$stateProvider
+
+	.state('login', {
+		url: '/login',
+		templateUrl: 'templates/login.html',
+		controller: 'LoginCtrl'
+	})
+
+	.state('train', {
+		url: '/train',
+		templateUrl: 'templates/train.html',
+		controller: 'TrainCtrl'
+	})
+
+	// if none of the above states are matched, use this as the fallback
+	$urlRouterProvider.otherwise('/login');
+
+});
---
+++ App/www/js/controllers.js
@@ -0,0 +1,9 @@
+angular.module('Fido.controllers', [])
+
+.controller('LoginCtrl', function($scope, $cordovaBarcodeScanner,$location) {
+	
+})
+
+.controller('TestCtrl', function($scope, $cordovaBarcodeScanner,$location) {
+	
+});
