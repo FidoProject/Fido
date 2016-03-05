e71fa510f462c156cce49a521dca04b26de3ef6d
Michael Truell <truell20@gmail.com>
2016-03-05 14:00:49 -0500

    Added in new buttons

--- App/www/js/controllers.js
+++ App/www/js/controllers.js
@@ -3,12 +3,9 @@ angular.module('Fido.controllers', ['ngCordova', 'Fido.services'])
 .controller('LoginCtrl', function($scope, $ionicPlatform, $fidoConnection, $state) {
 	$scope.connectToFido = function() {
 		$ionicPlatform.ready(function() {
-			console.log("Ready");
-			
 			$fidoConnection.init();
 			$fidoConnection.connect(function() {
 				console.log("Did connect");
-				console.log("Move to location");
 				$state.transitionTo('train');
 			}, function() {
 				console.log("Failed to connect");
@@ -18,21 +15,5 @@ angular.module('Fido.controllers', ['ngCordova', 'Fido.services'])
 })
 
 .controller('TrainCtrl', function($scope, $fidoConnection) {
-	console.log("Did move");
-	$scope.reward = function() {
-		console.log("reward")
-		$fidoConnection.sendReward(1);
-	};
-	$scope.punish = function() {
-		console.log("punish")
-		$fidoConnection.sendReward(-1);
-	};
-	$scope.neutral = function() {
-		console.log("neutral")
-		$fidoConnection.sendReward(0);
-	};
-	$scope.end = function() {
-		console.log("end")
-		$fidoConnection.sendReward(-2);
-	};
+	$scope.$fidoConnection = $fidoConnection;
 });
