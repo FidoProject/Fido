7aeaf774052f31985e1c706e1a6b8e5212778d78
joshuagruenstein <joshuagruenstein@gmail.com>
2016-03-06 01:25:53 -0500

    prettify js

--- App/www/js/app.js
+++ App/www/js/app.js
@@ -1,20 +1,10 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('Fido', ['ionic', 'ngCordova', 'Fido.controllers'])
 
 .run(function($ionicPlatform) {
 	$ionicPlatform.ready(function() {
 		if(window.cordova && window.cordova.plugins.Keyboard) {
-			// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-			// for form inputs)
 			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-			// Don't remove this line unless you know what you are doing. It stops the viewport
-			// from snapping when text inputs are focused. Ionic handles this internally for
-			// a much nicer keyboard experience.
 			cordova.plugins.Keyboard.disableScroll(true);
 		}
 		if(window.StatusBar) {
@@ -25,10 +15,6 @@ angular.module('Fido', ['ionic', 'ngCordova', 'Fido.controllers'])
 
 .config(function($stateProvider, $urlRouterProvider) {
 
-	// Ionic uses AngularUI Router which uses the concept of states
-	// Learn more here: https://github.com/angular-ui/ui-router
-	// Set up the various states which the app can be in.
-	// Each state's controller can be found in controllers.js
 	$stateProvider
 
 	.state('login', {
