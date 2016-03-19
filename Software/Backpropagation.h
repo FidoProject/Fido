15db33134ec4cb679da078abd21933636485ffba
FlyingGraysons <hmwildermuth@gmail.com>
2016-03-18 21:57:27 -0400

    Docs for Backpropagation.cpp

--- Software/Backpropagation.h
+++ Software/Backpropagation.h
@@ -21,36 +21,43 @@ namespace net {
 		 * \param maxIterations_ after this number of training iterations, a net will stop being trained no matter what
 		 */
 		Backpropagation(double learningRate_, double momentumTerm_, double targetErrorLevel_, int maximumIterations_);
-		
-		///
-		//! Initialize empty Backpropagation object
-		///
+
+		/**
+		 * \brief Initialize empty Backpropagation object
+		 */
 		Backpropagation();
 
-		///
-		//! Loads a Backpropagation object from a file
-		///
+		/**
+		 * \brief Loads a Backpropagation object from a file
+		 * \param filename the filename of the file being imported
+		 */
 		explicit Backpropagation(std::string filename);
 
-		///
-		//! Loads a Backpropagation object using an input stream
-		///
+		/**
+		 * \brief Loads a Backpropagation object using an input stream
+		 * \param input a pointer to the input stream that you want to form a neural network from
+		 */
 		explicit Backpropagation(std::ifstream *input);
 
-		///
-		//! Stores a Backpropagation object in the specified file
-		///
+		/**
+		 * \brief Stores a Backpropagation object in the specified file
+		 * \param filename the filename of the output file
+		 */
 		void storeBackpropagation(std::string filename);
 
-		///
-		//! Stores a Backpropagation object using specified ofstream. Useful for appending a network to the end of a file without overwriting it. WARNING: will not close ofstream.
-		///
+		/**
+		 * \brief Stores a Backpropagation object using specified ofstream.
+		 *
+		 * Useful for appending a network to the end of a file without overwriting it. WARNING: will not close ofstream.
+		 *
+		 * \param output pointer to the output stream which the neural network will be written to
+		**/
 		void storeBackpropagationWithStream(std::ofstream *output);
 
-		/** 
+		/**
 		 * \brief Trains a neural network on a training set.
 		 *
-		 * Edits the weights of the neural network until its error in predicting the correctOutput of each input reaches the value of targetErrorLevel 
+		 * Edits the weights of the neural network until its error in predicting the correctOutput of each input reaches the value of targetErrorLevel
 		 * or the number of training cycles reaches the value of maximumIterations.
 		 * NOTE: If learning rate is not low enough, the weights of the neural network may got to infinity due to the nature of backpropagation.
 		 *
@@ -60,55 +67,62 @@ namespace net {
 		 */
 		void trainOnData(net::NeuralNet *network, const std::vector< std::vector<double> > &input, const std::vector< std::vector<double> > &correctOutput);
 
-		//! Gets the output of the neural network, calculates the error of each neuron, and edits the weights of the neurons to reduce error
 		/**
+		 * \brief Gets the output of the neural network, calculates the error of each neuron, and edits the weights of the neurons to reduce error
+		 *
 		 * \param network the neural network to be trained
 		 * \param input the input fed to the neural network
 		 * \param correctOutput network is trained to output this when fed the input vector
 		 */
 		double trainOnDataPoint(net::NeuralNet *network, const std::vector<double> &input, const std::vector<double> &correctOutput);
 
-		///
-		//! Gets a map with the names of each activation function derivative as its keys and the available activation function derivatives as its values
-		///
+		/**
+		 * \brief Gets a map with the names of each activation function derivative as its keys and the available activation function derivatives as its values
+		 */
 		std::map<std::string, ActivationFunction> getDerivedActivationFunctionNames();
 
-		///
-		//! Sets the derivative of the activation function for the neurons of the hidden layers with the name of the activation funciton
-		///
+		/**
+		 * \brief Sets the derivative of the activation function for the neurons of the hidden layers with the name of the activation funciton
+		 *
+		 * \param name the name of the activation function that you want to set the hidden layers to
+		 */
 		void setDerivedHiddenActivationFunction(std::string name);
 
-		///
-		//! Sets the derivative of the activation function for the neurons of the output layer with the name of the activation funciton
-		///
+		/**
+		 * \brief Sets the derivative of the activation function for the neurons of the output layer with the name of the activation funciton
+		 *
+		 * \param name the name of the activation function that you want to set the output layer to
+		 */
 		void setDerivedOutputActivationFunction(std::string name);
 
-		///
-		//! Gets the name of the derivative of the activation function of the nuerons of the hidden layers
-		///
+		/**
+		 * \brief Gets the name of the derivative of the activation function of the nuerons of the hidden layers
+		 */
 		std::string getDerivedHiddenActivationFunctionName();
 
-		///
-		//! Gets the name of the derivative of the activation function of the neurons of the output layer
-		///
+		/**
+		 * \brief Gets the name of the derivative of the activation function of the neurons of the output layer
+		 */
 		std::string getDerivedOutputActivationFunctionName();
 
-		double learningRate, momentumTerm, targetErrorLevel;
-		int maxiumumIterations;
-		std::vector< std::vector< std::vector<double> > > lastChangeInWeight;
+		double learningRate; /**< The rate of learning, set by constructor */
+		double momentumTerm; /**< The term of momentum, set by constructor */
+		double targetErrorLevel; /**< The target error level, set by constructor */
+		int maximumIterations; /**< The maximum number of iterations, set by constructor */
+		std::vector< std::vector< std::vector<double> > > lastChangeInWeight; /**< The last change in weights */
 
 	private:
 		ActivationFunction hiddenActivationFunctionDerivative;
 		ActivationFunction outputActivationFunctionDerivative;
 
-		///
-		//! Uses a ifstream to initialize a backpropagation object
-		///
+		/**
+		 * \brief Uses a ifstream to initialize a backpropagation object
+		 */
 		void initWithStream(std::ifstream *input);
-		
-		///
-		//! Resets the lastchanginweight vector using a neural network is needed (NN is needed cause the number of layers, neurons, and weights are needed).
-		///
+
+		/**
+		 * \brief Resets the lastchanginweight vector using a neural network is needed (NN is needed cause the number of layers, neurons, and weights are needed).
+		 */
 		void resetLastChangeInWeight(net::NeuralNet *network);
 	};
 }
