1c6f4882dd0aaad1c46e0b4ba9c8ac1304d639cb
joshuagruenstein <joshuagruenstein@gmail.com>
2016-03-23 00:30:48 -0400

    neuron documentation

--- Software/Neuron.h
+++ Software/Neuron.h
@@ -6,34 +6,55 @@
 
 namespace net {
 
-	/* Class representing a single neuron.
+	/**
+	 * \brief Class representing a single neuron.
 	 *
-	 * Each neuron takes a variable number of inputs each assigned a weight and returns one output. Each neuron has a threshold value used to detemine if an output is produced (or to determine the magnitude of the neuron's output). A quality class by all accounts.
+	 * Each neuron takes a variable number of inputs each assigned
+	 * a weight and returns one output.  Each neuron has a threshold
+	 * value used to determine if an output is produced (or to
+	 * determine the magnitude of the neuron's output). A quality class
+	 * by all accounts.
 	 */
 	class Neuron {
 	public:
-		/* Create a neuron with randomly assigned weights.
+		/**
+		 * \brief Set of weights for the Neuron object.
+		 */
+		std::vector<double> weights;
+
+		/**
+		 * \brief Constructs a neuron with randomly assigned weights.
 		 *
-		 * Each weight is a random double between 0 and 1 
+		 * Each weight is a random double between 0 and 1.
+		 * 
+		 * \param numInputs the number of inputs to the neuron
 		 */
 		explicit Neuron(int numInputs);
 
-		// Create a neuron with given weights.
+		/**
+		 * \brief Constructs a neuron with given weights
+		 *
+		 * \param w the input weights from which to construct the neuron.
+		 */ 
 		explicit Neuron(std::vector<double> w);
 
-		/* Get outputs of neuron from given inputs.	
+		/**
+		 * \brief Gets outputs of neuron from given inputs.	
 		 *
 		 * The last weight is the threshold, as documented [here](http://www.ai-junkie.com/ann/evolved/nnt6.html).
 		 */
 		double getOutput(std::vector<double> inputs);
 		
-		// Set of weights for the Neuron object.
-		std::vector<double> weights;
-
-		// Randomizes the weights of the neuron
+		/**
+		 * \brief Randomizes the weights of the neuron.
+		 */
 		void randomizeWeights();
 
-		// Return a random weight between zero and one.
+		/**
+		 * \brief Generates a random input weight
+		 *
+		 * \return A random double between zero and one.
+		 */ 
 		static double randomWeight() {
 			return ((double)rand() / ((double)RAND_MAX / 2.0)) - 1.0;
 		} 
