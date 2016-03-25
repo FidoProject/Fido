e0fb68cce55e9e61d64e230a286f5d83ac574bdc
FlyingGraysons <hmwildermuth@gmail.com>
2016-03-24 18:02:32 -0700

    Re-add genetic algorithms
    fixes #9

---
+++ Software/Genetic/GeneticAlgo.cpp
@@ -0,0 +1,131 @@
+#include "GeneticAlgo.h"
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <ctime>
+
+using namespace gen;
+
+GeneticAlgo::GeneticAlgo(int populationSize_, float mutationRate_, float crossoverRate_, int numberOfElitismCopies_, std::vector<double>(*getPopulationFitness_)(std::vector<net::NeuralNet *> neuralNet)) {
+	populationSize = populationSize_;
+	mutationRate = mutationRate_;
+	crossoverRate = crossoverRate_;
+	getPopulationFitness = getPopulationFitness_;
+	numberOfElitismCopies = numberOfElitismCopies_;
+}
+
+void GeneticAlgo::crossover(net::NeuralNet *mom, net::NeuralNet *dad, net::NeuralNet *&offspring1, net::NeuralNet *&offspring2) {
+	float crossoverDeterminer = (float)rand() / (float)RAND_MAX;
+	if(crossoverDeterminer > crossoverRate) {
+		offspring1 = mom;
+		offspring2 = dad;
+		return;
+	}
+	std::vector<double> offspring1Weights;
+	std::vector<double> offspring2Weights;
+	std::vector<double> momWeights = mom->getWeights();
+	std::vector<double> dadWeights = dad->getWeights();
+
+	/// Crossover index must be a minimum of 1 and a maxiumum of the second to last index of the weights
+	int crossoverIndex = (rand() % (momWeights.size() - 2)) + 1;
+
+	for(int a = 0; a < crossoverIndex; a++) {
+		offspring1Weights.push_back(momWeights[a]);
+		offspring2Weights.push_back(dadWeights[a]);
+	}
+	for(int a = crossoverIndex; a < momWeights.size(); a++) {
+		offspring1Weights.push_back(dadWeights[a]);
+		offspring2Weights.push_back(momWeights[a]);
+	}
+
+	offspring1 = new net::NeuralNet(mom);
+	offspring1->setWeights(offspring1Weights);
+	offspring2 = new net::NeuralNet(dad);
+	offspring2->setWeights(offspring2Weights);
+
+	std::vector<double>().swap(offspring1Weights);
+	std::vector<double>().swap(offspring2Weights);
+	std::vector<double>().swap(momWeights);
+	std::vector<double>().swap(dadWeights);
+}
+
+void GeneticAlgo::mutate(net::NeuralNet *net) {
+	std::vector<double> weights = net->getWeights();
+
+	for(int a = 0; a < weights.size(); a++) {
+		float mutationDeterminer = (float)rand() / (float)RAND_MAX;
+		if(mutationDeterminer <= mutationRate) {
+			weights[a] = net::Neuron::randomWeight();
+		}
+	}
+
+	net->setWeights(weights);
+
+	std::vector<double>().swap(weights);
+}
+
+
+net::NeuralNet* GeneticAlgo::selectNNBasedOnFitness() {
+	double totalFitnessOfPopulation = 0;
+	for(int a = 0; a < fitnesses.size(); a++) totalFitnessOfPopulation += fitnesses[a];
+	double cutOff = (((double)rand() / (double)RAND_MAX) * totalFitnessOfPopulation);
+
+	double fitnessCounter = 0;
+	for (int a = 0; a < fitnesses.size(); a++) {
+		fitnessCounter += fitnesses[a];
+		if(fitnessCounter >= cutOff) {
+			return population[a];
+		}
+	}
+	std::cout << "There was an error in selecting a network based on fitness\n";
+	throw 1;
+}
+
+void GeneticAlgo::createNextGeneration() {
+	std::vector<net::NeuralNet *> nextGeneration;
+
+	int mostFitIndex = 0;
+	for(int a = 1; a < fitnesses.size(); a++) {
+		if(fitnesses[a] > fitnesses[mostFitIndex]) mostFitIndex = a;
+	}
+	for(int a = 0; a < numberOfElitismCopies; a++) nextGeneration.push_back(population[mostFitIndex]);
+	std::cout << "Highest: " << fitnesses[mostFitIndex] << "\n";
+
+	while(nextGeneration.size() < populationSize) {
+		net::NeuralNet *parent1 = selectNNBasedOnFitness(), *parent2 = selectNNBasedOnFitness();
+		net::NeuralNet *baby1, *baby2;
+
+		crossover(parent1, parent2, baby1, baby2);
+		mutate(baby1);
+		mutate(baby2);
+
+		nextGeneration.push_back(baby1);
+		nextGeneration.push_back(baby2);
+	}
+	std::vector<net::NeuralNet *>().swap(population);
+	population = nextGeneration;
+	std::vector<net::NeuralNet *>().swap(nextGeneration);
+}
+
+net::NeuralNet* GeneticAlgo::getBestNeuralNetwork(int numberOfGenerations, net::NeuralNet *modelNetwork) {
+	population.clear();
+	fitnesses.clear();
+
+	population.push_back(modelNetwork);
+	for(int a = 0; a < populationSize-1; a++) population.push_back(new net::NeuralNet(modelNetwork));
+
+	fitnesses = getPopulationFitness(population);
+
+	for(int a = 0; a < numberOfGenerations; a++) {
+		std::cout << "Generation: " << a << "\n";
+
+		createNextGeneration();
+		fitnesses = getPopulationFitness(population);
+	}
+
+	int mostFitIndex = 0;
+	for(int a = 1; a < fitnesses.size(); a++)if(fitnesses[a] > fitnesses[mostFitIndex]) mostFitIndex = a;
+	return population[mostFitIndex];
+}
---
+++ Software/Genetic/TicTacToe.cpp
@@ -0,0 +1,297 @@
+#include "TicTacToe.h"
+
+#include <thread>
+#include <iostream>
+
+#include "GeneticAlgo.h"
+
+std::vector<net::NeuralNet *> TicTacToe::randomSet = std::vector<net::NeuralNet*>(0);
+
+net::NeuralNet* TicTacToe::getBestPlayer(int numberOfIterations) {
+	gen::GeneticAlgo geneticAlgo(8, 0.2, 0.6, 5, getPopulationFitnesses);
+
+	net::NeuralNet *modelNet = new net::NeuralNet(9, 1, 4, 9, "sigmoid");
+
+	///for(int a = 0; a < 250; a++) randomSet.push_back(new net::NeuralNet(9, 1, 4, 9, "sigmoid"));
+
+	return geneticAlgo.getBestNeuralNetwork(numberOfIterations, modelNet);
+}
+
+std::vector<double> TicTacToe::getPopulationFitnesses(std::vector<net::NeuralNet *> players) {
+	std::vector< std::vector<double>* > scores;
+	std::vector<std::thread> threads;
+	int numThreads = 4;
+
+	int sizePerSubgroup = (int)players.size() / numThreads;
+
+	for(int a = 0; a < numThreads; a++) {
+		scores.push_back(new std::vector<double>(0));
+		if(a == numThreads-1) {
+			threads.push_back(std::thread(getPlayerFitnessesThread, std::vector<net::NeuralNet *>(players.begin() + sizePerSubgroup*a, players.end()), scores[a]));
+		} else {
+			threads.push_back(std::thread(getPlayerFitnessesThread, std::vector<net::NeuralNet *>(players.begin() + sizePerSubgroup*a, players.begin() + sizePerSubgroup*(a + 1)), scores[a]));
+		}
+	}
+	for(int a = 0; a < numThreads; a++) threads[a].join();
+
+	std::vector<double> returnVector;
+	for(int a = 0; a < scores.size(); a++) for(int b = 0; b < scores[a]->size(); b++) returnVector.push_back((*scores[a])[b]);
+
+	int mostFitIndex = 0;
+	for(int a = 0; a < returnVector.size(); a++) if(returnVector[a] > returnVector[mostFitIndex]) mostFitIndex = a;
+	players[mostFitIndex]->storeNet("C:/Users/Michael Truell/Documents/Fido/Software/PastNN/TTTcurrentgenerationbest.txt");
+
+	return returnVector;
+}
+
+void TicTacToe::getPlayerFitnessesThread(std::vector<net::NeuralNet *> players, std::vector<double> *fitnesses) {
+	/// Elo Rating System
+	for(int a = 0; a < players.size(); a++) {
+		double score = TicTacToe::getScoreAgainstRandomPlayers(players[a], 40);
+		std::cout << "Score: " << score << "\n";
+		fitnesses->push_back(score);
+	}
+
+	for(int a = 0; a < fitnesses->size(); a++) (*fitnesses)[a] = pow((*fitnesses)[a], 2);
+}
+
+int TicTacToe::getOutcomeOfGame(net::NeuralNet *player1, net::NeuralNet *player2) {
+	std::vector< std::vector<int> > board;
+	initializeBoard(&board);
+
+	int timesAround = 0;
+	while(timesAround < 2) {
+		executeMove(player1, &board, 1);
+		executeMove(player2, &board, -1);
+		timesAround++;
+	}
+	while(hasGameEnded(board) == false) {
+		executeMove(player1, &board, 1);
+		if(hasGameEnded(board) == true) break;
+		executeMove(player2, &board, -1);
+	}
+
+	/// Check if someone filled up a row
+	for(int a = 0; a < board.size(); a++) {
+		int initialValue = board[a][0];
+		bool isRowOneValue = true;
+		for(int b = 1; b < board[a].size(); b++) {
+			if(initialValue != board[a][b]) isRowOneValue = false;
+		}
+		if(isRowOneValue == true) {
+			if(initialValue == 1) return 1;
+			else if(initialValue == -1) return 2;
+		}
+	}
+
+	/// Check if someone filled up a column
+	for(int a = 0; a < board[0].size(); a++) {
+		int initialValue = board[0][a];
+		bool isColumOneValue = true;
+		for(int b = 1; b < board.size(); b++) {
+			if(initialValue != board[b][a]) isColumOneValue = false;
+		}
+		if(isColumOneValue == true) {
+			if(initialValue == 1) return 1;
+			else if(initialValue == -1) return 2;
+		}
+	}
+
+	/// Check if someone filled a diagonal
+	int initialValue = board[0][0];
+	bool isDiagonalOneValue = true;
+	for(int a = 1; a < board.size(); a++) if(board[a][a] != initialValue) isDiagonalOneValue = false;
+	if(isDiagonalOneValue == true) {
+		if(initialValue == 1) return 1;
+		else if(initialValue == -1) return 2;
+	}
+
+	initialValue = board[0][board.size()-1];
+	isDiagonalOneValue = true;
+	for(int a = 1; a < board.size(); a++) if(board[a][(board.size() - 1) - a] != initialValue) isDiagonalOneValue = false;
+	if(isDiagonalOneValue == true) {
+		if(initialValue == 1) return 1;
+		else if(initialValue == -1) return 2;
+	}
+
+	return 0;
+}
+
+void TicTacToe::executeMove(net::NeuralNet *player, std::vector< std::vector<int> > *board, int playerNumber) {
+	int bestRow = 0, bestColum = 0;
+	double bestScore = -9999999999;
+
+	std::vector<double> oneDBoard;
+	for(int a = 0; a < board->size(); a++) {
+		for(int b = 0; b < (*board)[a].size(); b++) {
+			if((*board)[a][b] == 0) oneDBoard.push_back(0);
+			else if((*board)[a][b] == playerNumber) oneDBoard.push_back(1);
+			else oneDBoard.push_back(-1);
+		}
+	}
+
+	for(int a = 0; a < oneDBoard.size(); a++) {
+		if(oneDBoard[a] == 0) {
+			oneDBoard[a] = 1;
+			double score = player->getOutput(oneDBoard)[0];
+			if(score > bestScore) {
+				bestScore = score;
+				bestRow = floor(a / 3);
+				bestColum = a % 3;
+			}
+			oneDBoard[a] = 0;
+		}
+	}
+	(*board)[bestRow][bestColum] = playerNumber;
+}
+
+void TicTacToe::initializeBoard(std::vector< std::vector<int> > *board) {
+	for(int a = 0; a < 3; a++) {
+		std::vector<int> row;
+		for(int b = 0; b < 3; b++) row.push_back(0);
+		board->push_back(row);
+	}
+}
+
+bool TicTacToe::hasGameEnded(std::vector< std::vector<int> > board) {
+	bool foundEmptySpace = false;
+	for(int a = 0; a < board.size(); a++) for(int b = 0; b < board[a].size(); b++) if(board[a][b] == 0) foundEmptySpace = true;
+	if(foundEmptySpace == false) return true;
+
+	/// Check if someone filled up a row
+	for(int a = 0; a < board.size(); a++) {
+		int initialValue = board[a][0];
+		if(initialValue != 0) {
+			bool isRowOneValue = true;
+			for(int b = 1; b < board[a].size(); b++) {
+				if(initialValue != board[a][b]) isRowOneValue = false;
+			}
+			if(isRowOneValue == true) return true;
+		}
+	}
+
+
+	/// Check if someone filled up a column
+	for(int a = 0; a < board[0].size(); a++) {
+		int initialValue = board[0][a];
+		if(initialValue != 0) {
+			bool isColumOneValue = true;
+			for(int b = 1; b < board.size(); b++) {
+				if(initialValue != board[b][a]) isColumOneValue = false;
+			}
+			if(isColumOneValue == true) return true;
+		}
+	}
+
+	/// Check if someone filled up a diagonal
+	int initialValue = board[0][0];
+	if(initialValue != 0) {
+		bool isDiagonalOneValue = true;
+		for(int a = 1; a < board.size(); a++) if(board[a][a] != initialValue) isDiagonalOneValue = false;
+		if(isDiagonalOneValue == true) return true;
+	}
+	initialValue = board[0][board.size()-1];
+	if(initialValue != 0) {
+		bool isDiagonalOneValue = true;
+		for(int a = 1; a < board.size(); a++) if(board[a][(board.size() - 1) - a] != initialValue) isDiagonalOneValue = false;
+		if(isDiagonalOneValue == true) return true;
+	}
+	return false;
+}
+
+void TicTacToe::printBoard(std::vector< std::vector<int> > board) {
+	for(int a = 0; a < board.size(); a++) {
+		for(int b = 0; b < board[a].size(); b++) {
+			if(board[a][b] == 0) std::cout << "| ";
+			else if(board[a][b] == 1) std::cout << "|X";
+			else if(board[a][b] == -1) std::cout << "|O";
+		}
+		std::cout << "|\n";
+	}
+}
+
+void TicTacToe::playVisualGame(net::NeuralNet *player1, net::NeuralNet *player2) {
+	std::vector< std::vector<int> > board;
+	initializeBoard(&board);
+
+	bool didOneMakeMove = true;
+
+	while(hasGameEnded(board) == false && didOneMakeMove == true) {
+		executeMove(player1, &board, 1);
+		printBoard(board);
+		std::cout << "\n";
+
+		if(hasGameEnded(board) == true) break;
+
+		executeMove(player2, &board, -1);
+		printBoard(board);
+		std::cout << "\n";
+	}
+}
+
+void TicTacToe::playAgainstHuman(net::NeuralNet *player) {
+	std::vector< std::vector<int> > board;
+	initializeBoard(&board);
+
+	while(hasGameEnded(board) == false) {
+		executeMove(player, &board, 1);
+		printBoard(board);
+
+		if(hasGameEnded(board) == true) break;
+
+		int row, colum;
+		std::cout << "Enter your row: ";
+		std::cin >> row;
+		std::cout << "Enter your colum: ";
+		std::cin >> colum;
+		if(board[row][colum] == 0) board[row][colum] = -1;
+		printBoard(board);
+	}
+}
+
+std::vector<double> TicTacToe::prepareBoardForPlayerInput(std::vector< std::vector<int> > board, int playerNumber) {
+	std::vector<double> returnVector;
+	for(int a = 0; a < board.size(); a++) {
+		for(int b = 0 ; b < board[a].size(); b++) {
+			if(board[a][b] == 0) returnVector.push_back(0);
+			else if(board[a][b] == playerNumber) returnVector.push_back(1);
+			else returnVector.push_back(-1);
+		}
+	}
+
+	return returnVector;
+}
+
+double TicTacToe::getScoreAgainstRandomPlayers(net::NeuralNet *player, int numberOfGames) {
+	double score = 0;
+	for(int a = 0; a < numberOfGames; a++) {
+		net::NeuralNet random = net::NeuralNet(9, 1, 4, 12, "sigmoid");
+		if(a < (int)(numberOfGames / 2.0)) {
+			int outcome = getOutcomeOfGame(player, &random);
+			if(outcome == 1) score++;
+			else if(outcome == 0) score += 0.5;
+		} else {
+			int outcome = getOutcomeOfGame(&random, player);
+			if(outcome == 2) score++;
+			else if(outcome == 0) score += 0.5;
+		}
+	}
+	return score;
+}
+
+// Pits a neural network against a set of neural networks in tic tac toe and returns the number of wins
+double TicTacToe::getScoreAgainstSetOfPlayers(net::NeuralNet *player, std::vector<net::NeuralNet *> networks) {
+	double score = 0;
+	for(int a = 0; a < networks.size(); a++) {
+		if(a < (int)(networks.size() / 2.0)) {
+			int outcome = getOutcomeOfGame(player, networks[a]);
+			if(outcome == 1) score++;
+			else if(outcome == 0) score += 0.5;
+		} else {
+			int outcome = getOutcomeOfGame(networks[a], player);
+			if(outcome == 2) score++;
+			else if(outcome == 0) score += 0.5;
+		}
+	}
+	return score;
+}
