b4224e4a1ea829e2e3b46619926fb06f0e994c28
Michael Truell <truell20@gmail.com>
2016-04-17 11:27:26 -0400

    Moved Layer to class

--- include/Layer.h
+++ include/Layer.h
@@ -13,7 +13,8 @@ namespace net {
 	/**
 	 * \brief A layer in a NeuralNet.
 	 */
-	struct Layer {
+	class Layer {
+	public:
 		/**
 		 * \brief The activation function for the neurons of this layer
 		 *
@@ -29,9 +30,7 @@ namespace net {
 		/**
 		 * \brief Initialize a layer with the default sigmoid activation function.
 		 */
-		Layer() {
-			activationFunction = sigmoid;
-		}
+		Layer();
 
 		/**
 		 * \brief Initialize a layer from a saved text file
@@ -41,52 +40,19 @@ namespace net {
 		 *
 		 * \param input the text file for the Layer object to be initialized from
 		 */
-		explicit Layer(std::ifstream *input) {
-			int numNeurons, numWeights;
-			std::string activationFunctionName;
-			*input >> numNeurons >> numWeights;
-			*input >> activationFunctionName;
-
-			setActivationFunctionWithName(activationFunctionName);
-
-			for (int a = 0; a < numNeurons; a++) {
-				std::vector<double> weights;
-				for (int b = 0; b < numWeights; b++) {
-					double weight;
-					*input >> weight;
-					weights.push_back(weight);
-				}
-				neurons.push_back(Neuron(weights));
-			}
-		}
+		explicit Layer(std::ifstream *input);
 
 		/**
 		 * \brief Sets the activation function to the given string
 		 *
 		 * \param name the name of the activation function
 		 */
-		void setActivationFunctionWithName(std::string name) {
-			std::map<std::string, ActivationFunction> nameMap = getActivationFunctionNameMap();
-			std::map<std::string, ActivationFunction>::iterator nameIterator = nameMap.find(name);
-			if(nameIterator != nameMap.end()) {
-				activationFunction = getActivationFunctionNameMap()[name];
-			} else {
-				std::cout << "The activation function \"" << name << "\" does not exist! Throwing error!\n";
-				throw 1;
-			}
-		}
-
+		void setActivationFunctionWithName(std::string name);
 
 		/**
 		 * \brief Gets the name of the layer's activation function
 		 */
-		std::string getActivationFunctionName() {
-			std::map<std::string, ActivationFunction> nameMap = getActivationFunctionNameMap();
-			for (std::map<std::string, ActivationFunction>::iterator a = nameMap.begin(); a != nameMap.end(); ++a) if (a->second == activationFunction) return a->first;
-
-			throw 1;
-		}
-
+		std::string getActivationFunctionName();
 
 		/**
 		 * \brief Store the layer to a text file
@@ -95,18 +61,7 @@ namespace net {
 		 *
 		 * \param output the text file for the layer object to be written to
 		 */
-		void store(std::ofstream *output) {
-			// All the neurons in the same layer have the same number of weights
-			// so that is stored once along with the number of neurons in the layer
-			// and the activation function name of the layer
-			*output << neurons.size() << " " << neurons[0].weights.size() << " " << getActivationFunctionName() << "\n";
-			for (unsigned int a = 0; a < neurons.size(); a++) {
-				for (unsigned int b = 0; b < neurons[a].weights.size(); b++) {
-					*output << neurons[a].weights[b] << " ";
-				}
-				*output << "\n";
-			}
-		}
+		void store(std::ofstream *output);
 
 		/**
 		 * \brief Gets the map of activation function name to ActivationFunction
