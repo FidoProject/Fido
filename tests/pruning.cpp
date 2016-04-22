0156d74b7b051467d3ead26741ca223632e2a000
Michael Truell <truell20@gmail.com>
2016-04-21 21:18:26 -0400

    Less latent pruning test

--- tests/pruning.cpp
+++ tests/pruning.cpp
@@ -8,16 +8,16 @@
 // Constants set for testing, could be anything
 const static double ERROR_ALLOWANCE = 0.1;
 const static int HIDDEN_LAYERS(1);
-const static int NEURONS(80);
-const static int MAX_FINAL_NEURONS(14);
-const static std::vector< std::vector<double> > input = { {1}, {2}, {5}, {6}, {8} };
-const static std::vector< std::vector<double> > correctOutput = { {2}, {4}, {10}, {12}, {16} };
+const static int NEURONS(20);
+const static int MAX_FINAL_NEURONS(15);
+const static std::vector< std::vector<double> > input = { {1}, {2}, {3} };
+const static std::vector< std::vector<double> > correctOutput = { {2}, {4}, {6} };
 
 TEST_CASE("Test pruning", "[pruner]") {
 	net::NeuralNet neuralNetwork = net::NeuralNet(input[0].size(), correctOutput[0].size(), HIDDEN_LAYERS, int(NEURONS/double(HIDDEN_LAYERS+1)), "sigmoid");
 	neuralNetwork.setOutputActivationFunction("simpleLinear");
 
-	net::Pruner pruner = net::Pruner(0.1, 0, -1, 14000, 1000);
+	net::Pruner pruner = net::Pruner(0.1, 0, -1, 1000, 100);
 	pruner.train(&neuralNetwork, input, correctOutput);
 
 	for (std::vector<double> current: input) {
