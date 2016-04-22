30bec5829924725b63f8415504e562a1d71ad3b6
Michael Truell <truell20@gmail.com>
2016-04-21 21:17:58 -0400

    Add getting of best network to GA example

--- examples/GeneticAlgoExample.cpp
+++ examples/GeneticAlgoExample.cpp
@@ -9,14 +9,15 @@ std::vector<double> getPopulationFitness(const std::vector<net::NeuralNet> &popu
     for(double input = 1; input < 4; input++) {
       totalError += pow(input*2 - network.getOutput({input})[0], 2);
     }
-		fitnesses.push_back(1/totalError);
+    fitnesses.push_back(1/totalError);
   }
 
   return fitnesses;
 }
 
 int main() {
-	srand(time(NULL));
+  srand(time(NULL));
   gen::GeneticAlgo geneticAlgo = gen::GeneticAlgo(20, 0.4, 0.5, 10, getPopulationFitness);
-	net::NeuralNet modelNetwork = net::NeuralNet(1, 1, 1, 4, "simpleLinear");
+  net::NeuralNet modelNetwork = net::NeuralNet(1, 1, 1, 4, "simpleLinear");
+  net::NeuralNet bestNetwork = geneticAlgo.getBestNeuralNetwork(600, modelNetwork);
 }
